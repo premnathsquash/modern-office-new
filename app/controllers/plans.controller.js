9cce28f52fa272f2e82443642624b73890fd5347
premnath <premdevapp@gmail.com>
2022-03-22 11:59:21 +0530

    modern office new stripe 2

--- app/controllers/plans.controller.js
+++ app/controllers/plans.controller.js
@@ -7,22 +7,11 @@ exports.getplans = async(req, res) => {
   console.log(products)
   const intermidiate = [
     {
-      id: "prod_L0ZoffTtV9fd11",
+      id: "prod_L0ZoffTtV9fd11FRee",
       object: "product",
       active: true,
-      attributes: [],
-      created: 1647659304,
-      description: null,
-      images: [],
-      livemode: false,
-      metadata: {},
       name: "free",
-      package_dimensions: null,
-      shippable: null,
-      statement_descriptor: null,
-      tax_code: "txcd_10000000",
       type: "service",
-      unit_label: null,
       updated: 1647659304,
       url: null,
     },
