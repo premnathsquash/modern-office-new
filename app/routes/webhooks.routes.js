11d368b3487785e037c65aa8ee1fac5b741370f3
premnath <premdevapp@gmail.com>
2022-03-24 13:16:18 +0530

    changes made in vie on 23/3 5

--- app/routes/webhooks.routes.js
+++ app/routes/webhooks.routes.js
@@ -1,7 +1,7 @@
 const stripe = require("stripe");
 const express = require("express");
 const endpointSecret =
-  "whsec_f2f42c68330a0eeba357bb0868a31d962cef6e05109a70d0839bfdca9d7bb38f";
+  "whsec_B8gfV6NgpSxSxgmYxNUL1sYIDNG44DGW"; //whsec_B8gfV6NgpSxSxgmYxNUL1sYIDNG44DGW //whsec_f2f42c68330a0eeba357bb0868a31d962cef6e05109a70d0839bfdca9d7bb38f
 
 module.exports = function (app) {
   app.post(
