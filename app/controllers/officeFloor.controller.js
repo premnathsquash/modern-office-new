898b99980e76916adde8a145da07aa60b96dc896
premnath <premdevapp@gmail.com>
2022-03-30 17:44:17 +0530

    changes made in vie on 30/3 7

--- app/controllers/officeFloor.controller.js
+++ app/controllers/officeFloor.controller.js
@@ -49,7 +49,7 @@ exports.CreateOffice = async (req, res, next) => {
 exports.ListOffices= async (req, res, next) => {
   const { slug } = req.query;
   try{
-    const offices = await Office.find({slug: slug})
+    const offices = await Office.find({slug: slug}).populate('floors')
     return res.json([...offices]);
   }catch(err){
     res.status(500).send({ message: err });
