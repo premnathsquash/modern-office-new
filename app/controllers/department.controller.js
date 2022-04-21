b6edabbaf307ab8c932ddbbfd595148113075efa
premnath <premdevapp@gmail.com>
2022-04-21 18:39:27 +0530

    description for respondunt

--- app/controllers/department.controller.js
+++ app/controllers/department.controller.js
@@ -7,7 +7,7 @@ exports.createDepartment = async (req, res) => {
     const { department } = req.body;
     const user = await User.findOne({ _id: req.userId });
     const departments = new Departments({
-     name: department,
+      departments: department,
     });
     departments.save((err, data) => {
       if (err) {
@@ -30,10 +30,11 @@ exports.createDepartment = async (req, res) => {
     return res.json({ res: "Error in Department" });
   }
 };
+
 exports.getAllDepartment = async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.userId }).populate({path:"departments"});
-    return res.json(user.departments);
+    return res.json({departmentToggle: true, deptData: user.departments, count: user.departments.length});
   } catch (err) {
     return res.json({ res: "Error in Department" });
   }
@@ -46,6 +47,7 @@ exports.updateDepartment = async (req, res) => {
     return res.json({ res: "Error in Department" });
   }
 };
+
 exports.deleteDepartment = async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.userId });
--- app/models/department.model.js
+++ app/models/department.model.js
@@ -3,16 +3,19 @@ const mongoose = require("mongoose");
 const Department = mongoose.model(
   "Department",
   new mongoose.Schema({
-    name: String,
+    departments: String,
     status: {
       type: Boolean,
       default: true
     },
-    members: {
+    users: {
       type: Number,
       default: 0
     },
-    
+    createdAt: {
+      type: Date,
+      default: Date.now
+  }
   })
 );
 
