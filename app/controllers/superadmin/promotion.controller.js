b2b6c6298904cac2ad7d8510e7239518e13e171e
premnath <premdevapp@gmail.com>
2022-04-25 15:51:48 +0530

    change in promotion and vendor

--- app/controllers/superadmin/promotion.controller.js
+++ app/controllers/superadmin/promotion.controller.js
@@ -3,14 +3,77 @@ const db = require("../../models");
 const Vendor = db.vendor;
 const Promotion = db.promotion;
 
-exports.createVendor = async(req, res)=>{
-  return res
-  .status(200)
-  .send({ message: "Vendor created successfuly" });
-}
+exports.createVendor = async (req, res) => {
+  const {
+    companyName,
+    companyDescription,
+    companyCategories,
+    contactName,
+    contactNumber,
+  } = req.body;
+  const [image, image2] = req.files;
+  const vendor1 = new Vendor({
+    companyName,
+    companyDescription,
+    companyCategories,
+    contactName,
+    contactNumber,
+    companyImage: image.location ?? "",
+    contactImage: image2.location ?? "",
+  });
+  vendor1.save((err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+  });
+  return res.status(200).send({ message: "Vendor created successfuly" });
+};
 
-exports.createPromotion = async(req, res)=>{
-  return res
-  .status(200)
-  .send({ message: "promotion created successfuly" });
-}
+exports.createPromotion = async (req, res) => {
+  const {
+    description,
+    company,
+    coupon,
+    offer,
+    categories,
+    validTill,
+    link,
+    vendorId,
+  } = req.body;
+  let fileLocation;
+  if (req.file) {
+    const { location } = req.file;
+    fileLocation = location;
+  }
+  const vendor = await Vendor.findOne({ _id: vendorId });
+  const promotion1 = new Promotion({
+    image: fileLocation ?? "",
+    description,
+    company,
+    coupon,
+    offer,
+    categories,
+    validTill,
+    link,
+  });
+
+  promotion1.save(async (err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    await Vendor.findOneAndUpdate(
+      { _id: vendor._id },
+      { promotionIds: [...vendor.promotionIds, data._id] },
+      (err, data) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+      }
+    );
+  });
+
+  return res.status(200).send({ message: "promotion created successfuly" });
+};
--- app/models/promotion.model.js
+++ app/models/promotion.model.js
@@ -2,20 +2,22 @@ const mongoose = require("mongoose");
 
 const Promotion = mongoose.model(
   "Promotion",
-  new mongoose.Schema({
-    image: String,
-    description: String,
-    company: String,
-    coupon: String,
-    offer: String,
-    claimed: String,
-    categories: [String],
-    validTill: Date,
-    link: String,
-  },
-  {
-    timestamps: true
-  })
+  new mongoose.Schema(
+    {
+      image: String,
+      description: String,
+      company: String,
+      coupon: String,
+      offer: String,
+      claimed: { type: Number, default: 0 },
+      categories: [String],
+      validTill: Date,
+      link: String,
+    },
+    {
+      timestamps: true,
+    }
+  )
 );
 
 module.exports = Promotion;
