0825d7fb6cfd1756af73ab2042b04d62813dd08f
premnath <premdevapp@gmail.com>
2022-05-03 19:27:00 +0530

    new payfinite

--- app/controllers/admin/leaderboard.controller.js
+++ app/controllers/admin/leaderboard.controller.js
@@ -11,22 +11,23 @@ exports.list = async (req, res) => {
         return;
       }
       await LeaderBoard.find({ companyId: data.id }, (err1, data1) => {
-        if (err) {
-          res.status(500).send({ message: err });
+        if (err1) {
+          res.status(500).send({ message: err1 });
           return;
         }
         const result = data1.map(async(el)=>{
+          const interme = el.book.map(el1=>{
+            const {reedemInfo, to, from, bookedTime, bookId, coins, coinsSpent} = el1;
+            return ({
+              reedemInfo: reedemInfo ?? "", to: to ?? "", from: from ?? "", bookedTime: bookedTime ?? "", bookId, coins: coins ?? "", coinsSpent : coinsSpent ?? ""
+            })
+          })
           const intermediate = await Profile.findOne({_id: el.profileId})
           return ({
             id: el.id,
             teammemberName: `${intermediate.firstName} ${intermediate.lastName}`,
             teammemberDp: intermediate.dp,
-            coins: "",
-            coinsRedemed: "",
-            redemedInfo: {
-              dp: "",
-              off: ""
-            }
+            book: interme[0]
           })
         })
 
