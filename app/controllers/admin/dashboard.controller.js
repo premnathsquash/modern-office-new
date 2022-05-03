f9e80988b90624696e83aac04a1f6ba60a08524a
premnath <premdevapp@gmail.com>
2022-05-04 02:37:21 +0530

    new payfinite

--- app/controllers/admin/dashboard.controller.js
+++ app/controllers/admin/dashboard.controller.js
@@ -11,18 +11,30 @@ exports.seatinfo = async (req, res) => {
       if (err) {
         return res.status(500).send({ message: err });
       }
-      const office = await Office.find({ slug: data.slug }).populate({
-        path: "floors",
-      }).populate({ 
-        path: 'floors',
-        populate: {
-          path: 'Seats',
-        } 
-     })
-      console.log(office);
-      
-
-      return res.send("data1");
+      const office = await Office.find({ slug: data.slug })
+        .populate({
+          path: "floors",
+        })
+        .populate({
+          path: "floors",
+          populate: {
+            path: "Seats",
+          },
+        });
+      const seats = office
+        .map((el) => {
+          return el.floors;
+        })
+        .flat(2);
+      const seatArr = seats.map((el) => {
+        const [first] =(el.Seats.seats);
+        const objV = Object.values(first);
+        return (objV);
+      }).flat(2);
+      const arr = ["Wall", "Door", "Toilet", "Diagonal Wall"]
+      const seat1 = seatArr.filter(el=> !arr.includes(el.displayName))
+      const seat2 = seat1.filter(el=> el.timesBooked > 0)
+      return res.send({totalSeats: seat1, bookedSeats: seat2});
     });
   } catch (err) {
     return res.status(500).send({ message: error });
