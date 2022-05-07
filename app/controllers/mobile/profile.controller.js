183b3a9ed0d1e2b6ac55cace540fd283806d536f
premnath <premdevapp@gmail.com>
2022-05-07 16:38:11 +0530

    changes made

--- app/controllers/mobile/profile.controller.js
+++ app/controllers/mobile/profile.controller.js
@@ -1,3 +1,7 @@
+const { customAlphabet } = require("nanoid");
+const nanoid = customAlphabet("1234567890abcdefghijklmnABCDEFGHIJKLMN", 4);
+
+const { sendMail } = require("../../config/mailer");
 const db = require("../../models");
 const Profile = db.profile;
 
@@ -16,36 +20,78 @@ exports.updateProfileImage = async (req, res) => {
       await Profile.findOneAndUpdate(
         { _id: req.userId },
         { dp: fileLocation ?? "" },
-        {new: true},
+        { new: true },
         (err1, data1) => {
           if (err1) {
             res.status(500).send({ message: err1 });
             return;
           }
-          return res.status(200).send({username: `${data1.firstName} ${data1.lastName}`, email: data1.email,  image: data1.dp});
+          return res
+            .status(200)
+            .send({
+              username: `${data1.firstName} ${data1.lastName}`,
+              email: data1.email,
+              image: data1.dp,
+            });
         }
       );
     });
-
-    
   } catch (err) {
     return res.status(500).send({ res: "Something went wrong", error });
   }
 };
+
 exports.sendProfileEmailOtp = async (req, res) => {
+  const otp = nanoid();
   try {
-    console.log(req.userId);
-
-    return res.status(200).send("client updated successfully");
+    await Profile.findOneAndUpdate(
+      { _id: req.userId },
+      { otp: otp },
+      { new: true },
+      async (err1, data1) => {
+        if (err1) {
+          res.status(500).send({ message: err1 });
+          return;
+        }
+        await sendMail(
+          req.body.email,
+          "Hydesq – updated email",
+          null,
+          `${req.body.email} otp: ${otp}`,
+          null
+        );
+      }
+    );
+    return res.status(200).send("Otp sent to provided mail");
   } catch (err) {
     return res.status(500).send({ res: "Something went wrong", error });
   }
 };
+
 exports.verifyProfileEmailOtp = async (req, res) => {
   try {
-    console.log(req.userId);
-
-    return res.status(200).send("client updated successfully");
+    const { email, otp } = req.body;
+    await Profile.findOne({ _id: req.userId }, async (err, data) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      if (otp == data._doc.otp) {
+        await Profile.findOneAndUpdate(
+          { _id: req.userId },
+          { email: email },
+          (err1, data1) => {
+            if (err1) {
+              res.status(500).send({ message: err1 });
+              return;
+            }
+          }
+        );
+        return res.status(200).send("Email updated successfully");
+      } else {
+        return res.status(200).send("Otp mismatch");
+      }
+    });
   } catch (err) {
     return res.status(500).send({ res: "Something went wrong", error });
   }
