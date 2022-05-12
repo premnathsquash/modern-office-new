cef0f1739580f296da88abd705f1a8f9e3ed99d9
premnath <premdevapp@gmail.com>
2022-05-13 01:37:18 +0530

    changes

--- app/routes/web.notification.route.js
+++ app/routes/web.notification.route.js
@@ -1,8 +1,20 @@
+const { errors } = require("celebrate");
+
 module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
   app.ws("/echo", (ws, req) => {
     console.log("connection");
     ws.on("message", (msg) => {
       ws.send(msg);
     });
   });
+
+  app.use(errors());
 };
