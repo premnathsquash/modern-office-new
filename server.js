81a223c7a26216a93127a93748c50d907f4cd1de
premnath <premdevapp@gmail.com>
2022-05-13 00:16:43 +0530

    changes

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
+const ws = require("ws");
 const cors = require("cors");
-const { errors } = require('celebrate');
+const { errors } = require("celebrate");
 const dbConfig = require("./app/config/db.config");
 const db = require("./app/models");
 const initial = require("./createRoles");
 const PORT = process.env.PORT || 8080;
 
-const app = express();
+const server = express();
 
 var corsOptions = {
   origin: "*",
 };
-app.use(errors());
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  if (req.originalUrl.startsWith('/webhook')) {
+server.use(errors());
+server.use(cors(corsOptions));
+server.use((req, res, next) => {
+  if (req.originalUrl.startsWith("/webhook")) {
     next();
   } else {
     express.json()(req, res, next);
   }
 });
-app.use(express.urlencoded({ extended: true }));
+server.use(express.urlencoded({ extended: true }));
 
-require("./app/routes/auth.routes")(app);
-require("./app/routes/enquire.routes")(app);
-require("./app/routes/plans.routes")(app);
-require("./app/routes/webhooks.routes")(app);
-require("./app/routes/officeFloor.routes")(app);
-require("./app/routes/stripe.routes")(app);
-require("./app/routes/department.routes")(app);
-require("./app/routes/officeConfigure.routes")(app);
-require("./app/routes/attendance.route")(app);
-require("./app/routes/promotion.route")(app);
-require("./app/routes/bookings.route")(app);
-require("./app/routes/leaderboard.route")(app);
-require("./app/routes/dashboard.routes")(app);
-require("./app/routes/dashboard.routes")(app);
-require("./app/routes/reedeem.routes")(app);
-require("./app/routes/notification.route")(app);
-require("./app/routes/reports.route")(app);
+require("./app/routes/auth.routes")(server);
+require("./app/routes/enquire.routes")(server);
+require("./app/routes/plans.routes")(server);
+require("./app/routes/webhooks.routes")(server);
+require("./app/routes/officeFloor.routes")(server);
+require("./app/routes/stripe.routes")(server);
+require("./app/routes/department.routes")(server);
+require("./app/routes/officeConfigure.routes")(server);
+require("./app/routes/attendance.route")(server);
+require("./app/routes/promotion.route")(server);
+require("./app/routes/bookings.route")(server);
+require("./app/routes/leaderboard.route")(server);
+require("./app/routes/dashboard.routes")(server);
+require("./app/routes/dashboard.routes")(server);
+require("./app/routes/reedeem.routes")(server);
+require("./app/routes/reports.route")(server);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
   db.mongoose
     .connect(`${dbConfig.HOSTURL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
+      useFindAndModify: false,
     })
     .then(() => {
       console.log("Successfully connect to MongoDB.");
@@ -57,4 +57,10 @@ app.listen(PORT, () => {
       console.error("Connection error", err);
       process.exit();
     });
+});
+
+const wss = new ws.Server({ server });
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
 });
