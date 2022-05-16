6ccc73a065e16dd11e00d227bc0c7d339a517d00
premnath <premdevapp@gmail.com>
2022-05-16 11:52:29 +0530

    changes

--- app/controllers/admin/notification.controller.js
+++ app/controllers/admin/notification.controller.js
@@ -1,16 +1,40 @@
 const db = require("../../models");
 const User = db.user;
+const Notifications = db.notifications;
 
 exports.updateNotification = async (req, res) => {
   try {
-const companyNotification = await User.findOne({_id: req.userId}).populate({path: "notification"})
-  } catch (error) {}
+    const companyNotification = await User.findOne({
+      _id: req.userId,
+    }).populate({ path: "notification" });
+
+  
+    console.log(companyNotification);
+    
+  } catch (error) {
+    return res.status(500).send({ message: error });
+  }
 };
 
 exports.getNotification = async (req, res) => {
   try {
-const companyNotification = await User.findOne({_id: req.userId}).populate({path: "notification"})
-  } catch (error) {}
+    const companyNotification = await User.findOne({
+      _id: req.userId,
+    }).populate({ path: "notification" });
+    const result = {
+      id: companyNotification?.notification.id,
+      floorPlanNotification50: companyNotification?.notification.floorPlanNotification50,
+      floorPlanNotification80: companyNotification?.notification.floorPlanNotification80,
+      floorPlanNotification100: companyNotification?.notification.floorPlanNotification100,
+      profileNotificationUpdateName: companyNotification?.notification.profileNotificationUpdateName,
+      profileNotificationUpdateEmail: companyNotification?.notification.profileNotificationUpdateEmail,
+      pushNotifications: companyNotification?.notification.pushNotifications,
+      emailNotifications: companyNotification?.notification.emailNotifications,
+    }
+    return res.status(200).send(result);
+  } catch (error) {
+    return res.status(500).send({ message: error });
+  }
 };
 
 exports.check = async (ws, req) => {
