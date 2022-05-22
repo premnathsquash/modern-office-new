1edbf487868e29b8c559aa1eec837e4c48927271
Premnath <premnath@squashapps.com>
2022-05-22 18:11:59 +0530

    check

--- app/config/exportImportDocs.js
+++ app/config/exportImportDocs.js
@@ -3,6 +3,9 @@ require("dotenv").config()
 const aws = require("aws-sdk");
 const PDFDocument = require('pdfkit');
 const parse = require("csv-parse");
+const fs = require('fs');
+const fs1 = require('fs').promises;
+const path = require('path')
 
 const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
 const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
@@ -16,27 +19,45 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
-const exporter = async (data, fileName) => {
+const createPdf = async (data, fileName) => {
   try {
-    const doc = new PDFDocument();
-
-    doc.text("Text for your PDF");
+    const doc = new PDFDocument({ size: 'A4' });
+    const appDir = path.dirname(require.main.filename);
+    let writeStream = fs.createWriteStream(`${appDir}/${fileName}.pdf`)
+    doc.fontSize(25).text(data, 100, 100);
+    doc.save()
+    doc.pipe(writeStream);
     doc.end();
 
-    const params = {
-      key: fileName,
-      body: doc,
-      bucket: 'gu-export-files',
-      contentType: 'application/pdf',
-      ACL: "public-read-write"
-    }
-    s3.upload(params, function (err, response) {
+    writeStream.on('finish', async (err) => {
       if (err) {
-        console.log(err);
+        console.log("file creatation failed", err);
       }
-      return ("fileuploaded");
+      const data = await fs1.readFile(`${appDir}/${fileName}.pdf`, "binary");
 
-    });
+
+      const params = {
+        Key: `${fileName}`,
+        Body: Buffer.from(data),
+        Bucket: "gu-export-files",
+        ContentType: 'application/pdf',
+      }
+
+      const check = []
+
+      s3.upload(params, (err, data) => {
+        const value = new Promise((resolve, reject) => {
+          if (err) {
+            reject(err)
+          }
+          resolve(data.Location)
+        })
+        check.push(value)
+      });
+
+      console.log(check);
+    })
+
   } catch (error) {
     console.log(error);
     throw error;
@@ -58,5 +79,5 @@ const importer = async (file) => {
 
 module.exports = {
   importer,
-  exporter,
+  createPdf,
 };
--- app/controllers/admin/importexportfile.controller.js
+++ app/controllers/admin/importexportfile.controller.js
@@ -1,5 +1,10 @@
-const { exporter, importer } = require("../../config/exportImportDocs");
+const { customAlphabet } = require("nanoid");
+const nanoid = customAlphabet("1234567890abcdefghijklmnABCDEFGHIJKLMN", 10);
+const { createPdf, importer } = require("../../config/exportImportDocs");
 
 exports.exportPdfFile = async (req, res) => {
-    console.log("k");
+    const name = nanoid()
+    const file = await createPdf("check", name)
+    console.log(file);
+   
 }
